dc3b61bd41ed92141e616757d8a6ad322f67e6b6
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-07-13 15:53:33 +0700

    Add files via upload

---
+++ K4MBINGHITAM-deobfuscated.sh
@@ -0,0 +1,87 @@
+clear
+bi='[34;1m' #biru
+i='[32;1m' #ijo
+pur='[35;1m' #purple
+cy='[36;1m' #cyan
+me='[31;1m' #merah
+pu='[37;1m' #putih
+ku='[33;1m' #kuning
+echo
+echo
+echo
+clear
+echo
+echo $pur"=============≠====" $cy "======================"
+echo $i"#" $i"  AUTHOR"   :     $me"K4MBING H1T4M      "
+echo $i"#" $cy"  GMAIL    :"  $pu"rayyanalifi69@gmail.com"
+echo $i"#" $cy"  Github   :"  $pu"https://github.com/rayyanaa "
+echo $i"#" $cy"  TEAM    :"    $pu"Indonesia Security Team"
+echo $i"#" $cy"Thanks To :" $pu"Wannabe_ID,Iy Darksite,Hanz.id"
+echo $i"#" $cy"Thanks To:" $pu"IY Hari,Yazid Kherzakov,IY Low"
+echo $i"#" $cy"Thanks To" $pu"IY Ctr,Anthony Zippey,IY Grzly"
+echo $i"#" $i"  TEAM   :"  $me"2EASY4HACK"
+echo $pur"=============≠====" $cy "======================"
+sleep 2
+echo
+echo
+echo $ku"==> Menu Tools :"
+sleep 1
+echo $ku"<"$me"1"$ku">"$cy"Encrypt Bash"
+sleep 0.5
+echo $ku"<"$me"2"$ku">"$me"Keluar"
+sleep 0.5
+echo $ku"<"$me"3"$ku">"$cy"Informasi Tools Ini"
+sleep 0.5
+echo
+echo $me"╭─["$cy"Masukan pilihan anda"$me"]"
+read -p"╰─root@./KAMBINGHIT4M= " pil
+if [ $pil = 1 ]
+then
+clear
+echo $cy"Masukan Nama File Yang Ingin di Encrypt" $me"["$cy"CONTOH   Bash.sh"$me"]"
+read file
+echo $ku"Masukan Hasil File Yang Ingin Di Encript" $ku"["$me"CONTOH  Hasil.sh"$ku"]"
+read hasil
+bash-obfuscate /sdcard/$file -o /sdcard/$hasil
+echo $me"{"$cy"HASIL"$me"}" : /sdcard/$hasil $me"["$cy"CEK DI INTERNAL KALIAN"$me"]"
+echo
+echo $cy"Semoga Bermanfaat :)"
+fi
+if [ $pil = 2 ]
+then
+clear
+echo $cy"Terima Kasih CUK Sudah Pakai Tool ini"
+sleep 2
+echo $ku"AUTHOR :"$i" KAMBING HITAM"
+echo $ku"TEAM  :"$i" 2E4H"
+echo $ku"whatsapp :"$i" 0895414920813 "
+sleep 2
+echo $pur">> makasih Coek Yang Sudah Support Tool W<<"
+exit
+fi
+if [ $pil = 3 ]
+then
+clear
+echo $bi"Contact: "  $pu"0895414920813"
+sleep 1
+echo $pur"Pemrograman: "  $cy"Bash"
+sleep 1
+echo $ku"Karya: K4MBINGHITAM"
+sleep 1
+echo $i"Versi: v1"
+sleep 1
+echo $cy"Team:" $me"2EASY"$pu"4HACK"
+sleep 1
+echo $pur"Spesial To:"
+echo
+echo $cy"low"
+echo $cy"Hanz"
+echo $cy"Darksite"
+echo $cy"Wannabe_ID"
+echo $cy"N4F Cyber Team"
+echo $cy"Indonesia Security Team"
+echo $cy" And All Member 2EASY4HACK"
+echo $ku"Tunggu 5 detik"
+sleep 5
+sh K4MBINGHITAM.sh
+fi
